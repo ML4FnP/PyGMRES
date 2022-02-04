a377d199fb08b0dbc4f44f538ca7bcf3065082cb
Johannes Blaschke <jpblaschke@lbl.gov>
2022-02-03 16:37:38 -0800

    Add numba.jit

---
+++ PyGMRES/compiler/numba.py
@@ -0,0 +1,35 @@
+from functools import wraps
+from inspect   import signature
+
+from . import RC, Decorated
+
+RC().lock()
+
+if RC().enable_numba:
+    import numba
+
+
+def jit(**kwargs):
+    """
+    Conditional Numba compiler decorator that invokes the compiler iff
+    RC().enable_numba = True when decorator is invoked (i.e. when the decorated
+    function is first defined.)
+    """
+
+    def noop(func):
+        return func
+
+    def op(func):
+        _op = wraps(func)(
+            numba.jit(**kwargs)(
+                func
+            )
+        )
+        _op.__signature__ = signature(func)
+        Decorated().add(func)
+        return _op
+
+    if RC().enable_numba:
+        return op
+    else:
+        return noop
